6872d2bb707446d84365c5bb25712e0ec0765201
milinkorath <milinkorath@gmail.com>
2016-02-18 14:42:28 +0530

    add doc

--- main.go
+++ main.go
@@ -12,23 +12,29 @@ import (
 const (
 	Success = iota
 	Failed
+	msize=10 // setting maximum array size
 )
 
 func main() {
-	inputFile, err := ioutil.ReadFile("data/input.txt")
+	if len(os.Args) < 2 {
+		fmt.Println("input file is missing.")
+		os.Exit(Failed)
+	}
+	fname := os.Args[1]
+	inputFile, err := ioutil.ReadFile(fname)
 	if err != nil {
 		fmt.Printf(err.Error())
 		os.Exit(Failed)
 	}
 	contents := string(inputFile)
 	contents = strings.Trim(contents, "\n")
-	size := strings.Count(contents, "\n") + 1
+	size := strings.Count(contents, "\n") + 1  // actual size of the input matrix.Used for iteration
 	if !isSqureMatrix(size) {
 		fmt.Println("Matrix is not valid")
 		os.Exit(Failed)
 	}
 	s := int(math.Sqrt(float64(size)))
-	var matrix [10][10]int64 // max input matrix size is 10*10
+	var matrix [msize][msize]int64 // max input matrix size is 10*10
 	data := strings.Split(contents, "\n")
 	for _, value := range data {
 		lines := strings.Fields(value)
@@ -43,9 +49,9 @@ func main() {
 	result := multiply(matrix, tranMatrix, s)
 	printMatrix(result, "A*A`", s)
 }
-
-func transpose(matrix [10][10]int64, size int) [10][10]int64 {
-	var transpose [10][10]int64
+// to find the transpose of the matrix
+func transpose(matrix [msize][msize]int64, size int) [msize][msize]int64 {
+	var transpose [msize][msize]int64
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
 			transpose[i][j] = matrix[j][i]
@@ -53,9 +59,9 @@ func transpose(matrix [10][10]int64, size int) [10][10]int64 {
 	}
 	return transpose
 }
-
-func multiply(a, b [10][10]int64, size int) [10][10]int64 {
-	var multiply [10][10]int64
+// to mutiply two matrix
+func multiply(a, b [msize][msize]int64, size int) [msize][msize]int64 {
+	var multiply [msize][msize]int64
 	var sum int64
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
@@ -69,8 +75,8 @@ func multiply(a, b [10][10]int64, size int) [10][10]int64 {
 	}
 	return multiply
 }
-
-func printMatrix(a [10][10]int64, b string, size int) {
+// used to format the output
+func printMatrix(a [msize][msize]int64, b string, size int) {
 	fmt.Println(b)
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
@@ -80,6 +86,7 @@ func printMatrix(a [10][10]int64, b string, size int) {
 	}
 
 }
+// used to check square matrix
 func isSqureMatrix(size int) bool {
 	sqrt := int(math.Sqrt(float64(size)))
 	return sqrt*sqrt == size
